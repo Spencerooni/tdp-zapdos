67fa34c8d6db03dc1b3b24025af1f31ea594e092
Tom Strahan <W.Strahan@kainos.com>
2016-08-18 12:44:10 +0100

    Getting ready for spike test

--- TeamHyperAlphaWolfDarkZero/src/app/Application.java
+++ TeamHyperAlphaWolfDarkZero/src/app/Application.java
@@ -1,5 +1,11 @@
 package app;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 import EmployeeRelated.Employee;
 
 public class Application {
@@ -12,6 +18,30 @@ public class Application {
 		System.out.println(emp1.getEmpFirstName() + " " +
 		emp1.getEmpLastName());
 		
+		JFrame frame = new JFrame("The Spike");
+		frame.setSize(400, 400);
+		frame.setSize(400, 400);
+		
+		JButton button = new JButton("Submit");
+		button.setActionCommand("hi");
+		frame.add(button);
+		
+		frame.setVisible(true);
+		
+		//button.addActionListener(new ButtonCLickListener());
+		
+		
+		
 	}
 
+
+}
+
+final class ButtonClickListener implements ActionListener{
+    public void actionPerformed(ActionEvent e) {
+       String command = e.getActionCommand();  
+       if( command.equals( "hi" ))  {
+          System.out.println("");
+       }
+    }
 }
