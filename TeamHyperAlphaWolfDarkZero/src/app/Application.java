d17e9cf822610680a8e31e1cf7901c9973802c2f
Tom Strahan <W.Strahan@kainos.com>
2016-08-18 11:40:11 +0100

    Added a main Application class, currently contains quick test for Employee.

---
+++ TeamHyperAlphaWolfDarkZero/src/app/Application.java
@@ -0,0 +1,17 @@
+package app;
+
+import EmployeeRelated.Employee;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee emp1 = new Employee(1, "Tim", "Bob", "PA 88 66 22 A");
+		
+		System.out.println(emp1.getEmpFirstName() + " " +
+		emp1.getEmpLastName());
+		
+	}
+
+}
